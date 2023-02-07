f1082878e1f8f262a17c621bc2b8c1876a382d29
Максим Самисько <masechk@gmail.com>
2023-02-07 21:45:04 +0300

    add new project

---
+++ Modul3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String MyName;
+            MyName = "Max";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
