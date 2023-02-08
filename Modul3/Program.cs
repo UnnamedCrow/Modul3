89763b8b7bfaeb2ede89d2053227991d55541859
Максим Самисько <masechk@gmail.com>
2023-02-08 11:10:14 +0300

    Modul3-master: Simple Anketa

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -10,9 +10,14 @@ namespace Modul3
     {
         static void Main(string[] args)
         {
-            String MyName;
-            MyName = "Max";
-            Console.WriteLine(MyName);
+            String MyName = "Max";
+            byte MyAge = 31;
+            bool MyPet = true;
+            double MyShoeSize = 40.5;
+            Console.WriteLine("My name is {0}",MyName);
+            Console.WriteLine("My age is {0}", MyAge);
+            Console.WriteLine("Do I have a pet? {0}", MyPet);
+            Console.WriteLine("My shoe size is {0}", MyShoeSize);
             Console.ReadKey();
         }
     }
