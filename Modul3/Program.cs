b7dc1ac3cd826ae6b1dfbb467cfb53f2a784dfd7
Максим Самисько <masechk@gmail.com>
2023-04-22 12:26:08 +0300

    Homework-Unit 13.6.1: comparision of addition speed in to List and LinkedList

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -4,55 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Specialized;
 
 namespace Modul3
 {
     internal class Program
     {
-        enum DayOfWeek : byte
-        {
-            Monday = 1,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,  
-            Sunday
-            
-        }
+        public static List<string> wordsList= new List<string>();
+        public static LinkedList<string> wordsLinkedList = new LinkedList<string>();
         static void Main(string[] args)
         {
+            // Читаем файл
+            string text = File.ReadAllText("Text1.txt");
 
-            DayOfWeek firstDayOfWeek = DayOfWeek.Monday;
-            Console.Write(firstDayOfWeek);
-            // Ввод имени
-            Console.Write("Enter your name: ");
-            var Name = Console.ReadLine();
-            
-            // Ввод возраста
-            Console.Write("Enter your age: ");
-            var  Age = checked((byte) int.Parse(Console.ReadLine()));
+            // Достаём массив слов
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-            // Ввод даты рождения
-            Console.Write("Enter your birthdate: ");
-            var Birthdate = Console.ReadLine();
+            // Записываем в List
+            var stopWatch = Stopwatch.StartNew();
+            AddToList(words);
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
 
-            // Ввод любимого дня недели
-            Console.Write("Enter your favorite day of the week: ");
-            var FavoriteDay = (DayOfWeek) byte.Parse(Console.ReadLine());
+            // Записываем в LinkedList в конец
+            stopWatch.Restart();
+            AddToLinkedListLast(words);
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
 
-            // Ввод любимого цвета
-            Console.Write("Enter your favorite color: ");
-            var FavoriteColor = Console.ReadLine();
-
-            // Вывод полученых данных
-            Console.WriteLine("Your name is {0}, and your age is {1}", Name, Age);
-            Console.WriteLine($"Your favorite day of the week is {FavoriteDay}, and your favorite color is {FavoriteColor}");
-            Console.WriteLine("Your birthdate is " + Birthdate);      
-
-            // Ожидание ввода
+            // Записываем в LinkedList в начало
+            wordsLinkedList.Clear();
+            stopWatch.Restart();
+            AddToLinkedListFirst(words);
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
             Console.ReadKey();
-
+        }
+        public static void AddToList(string[] words)
+        {
+            foreach (string word in words)
+            {
+                wordsList.Add(word);
+            }
+        }
+        public static void AddToLinkedListLast(string[] words) 
+        {
+            foreach (string word in words)
+            {
+                wordsLinkedList.AddLast(word);
+            }
+        }
+        public static void AddToLinkedListFirst(string[] words)
+        {
+            foreach (string word in words)
+            {
+                wordsLinkedList.AddFirst(word);
+            }
         }
     }
 }
